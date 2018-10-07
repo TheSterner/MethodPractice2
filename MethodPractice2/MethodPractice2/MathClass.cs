dcae391c067bdd0588359d08404bfb7a924525a7
William B McAllister <blakemcallister77@gmail.com>
2018-10-07 15:31:53 -0400

    a

---
+++ MethodPractice2/MethodPractice2/MathClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice2
+{
+	public class MathClass
+	{
+		public static int AddUserNumber(int i)
+		{
+			return i + 10;
+		}
+
+		public static int AddUserNumber(decimal j)
+		{
+			decimal k = j * 5;
+			int f = Convert.ToInt32(k);
+			return f;
+		}
+
+		//I cannot get the method to take a string to an int.  I keep getting an error. 
+		//public static int AddUserNumber(string h )
+		//{
+		//	var g;
+		//	if (Int32.TryParse(h, out g))
+		//		//int b = g * 20;
+		//		return g;
+		//	else
+		//		Console.WriteLine("Could not convert to integer.");
+		}
+
+	}
+}
---
+++ MethodPractice2/MethodPractice2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int plusTwelve = MathClass.AddUserNumber(12);
+			Console.WriteLine(plusTwelve);
+			Console.ReadLine();
+			int secondMethod = MathClass.AddUserNumber(1.5m);
+			Console.WriteLine(secondMethod);
+			Console.ReadLine();
+			//string thirdMethod = MathClass.AddUserNumber("3");
+			//Console.WriteLine(thirdMethod);
+			//Console.ReadLine();
+
+
+		}
+	}
+}
